d48acf47e2b0fb3552e02616cf9972bb2baa511b
PratyushaThumiki <pratyusha.thumiki@gmail.com>
2018-06-26 20:01:41 +0530

    backend changes

---
+++ app/src/FreeTest/java/com/udacity/gradle/builditbigger/free/EndpointAsyncTaskTest.java
@@ -0,0 +1,45 @@
+package com.udacity.gradle.builditbigger.free;
+
+import android.support.test.rule.ActivityTestRule;
+
+import com.google.android.gms.ads.InterstitialAd;
+import com.udacity.gradle.builditbigger.EndpointAsyncTask;
+import com.udacity.gradle.builditbigger.MainActivity;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.Assert.*;
+
+
+public class EndpointAsyncTaskTest  implements EndpointAsyncTask.ProgressBarInterface {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule
+            = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testAsync() {
+        String result = null;
+
+        final EndpointAsyncTask endpointAsyncTask = new EndpointAsyncTask(this, new InterstitialAd(mActivityTestRule.getActivity()));
+
+        endpointAsyncTask.execute(mActivityTestRule.getActivity());
+
+        try {
+            result = endpointAsyncTask.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertTrue(result.length() > 0 && !result.equals("connect timed out"));
+    }
+
+    @Override
+    public void hideProgressBar(boolean hide) {}
+}
---
+++ app/src/PaidTest/java/com/udacity/gradle/builditbigger/paid/EndpointAsyncTaskTest.java
@@ -0,0 +1,41 @@
+package com.udacity.gradle.builditbigger.paid;
+
+import android.support.test.rule.ActivityTestRule;
+
+import com.udacity.gradle.builditbigger.EndpointAsyncTask;
+import com.udacity.gradle.builditbigger.MainActivity;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.concurrent.ExecutionException;
+
+
+public class EndpointAsyncTaskTest implements EndpointAsyncTask.ProgressBarInterface{
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule
+            = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testAsync() {
+        String result = null;
+
+        final EndpointAsyncTask endpointAsyncTask = new EndpointAsyncTask(this);
+
+        endpointAsyncTask.execute(mActivityTestRule.getActivity());
+
+        try {
+            result = endpointAsyncTask.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertTrue(result.length() > 0 && !result.equals("connect timed out"));
+    }
+
+    @Override
+    public void hideProgressBar(boolean hide) {}
+}
